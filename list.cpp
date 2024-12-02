9f7b6855d4d759361d313e10af9b156d10c33ebb
Bharathi S <92158598+sgbharti22@users.noreply.github.com>
2024-12-02 13:26:07 +0530

    Create list.cpp

---
+++ list.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <list>
+using namespace std;
+int main()
+{
+    // List -> internal implementation is doubly linkedlist
+    list<int> l;
+    l.push_back(34);
+    l.push_back(54);
+    l.push_front(11);
+    l.push_front(22);
+    l.push_back(674);
+    
+    l.emplace_back(111);
+    l.emplace_front(222);
+    
+    for(int i:l){   cout<<i<<"  ";} cout<<endl;
+    
+    // to check the size of the list
+    cout<<"size : "<<l.size()<<endl;
+    // erase(), clear(), begin(), end(), rbegin(), rend(), insert(), front(), back()
+    
+    return 0;
+}
